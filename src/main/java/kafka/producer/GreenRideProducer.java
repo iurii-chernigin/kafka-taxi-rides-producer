39bcb0391b72cc30843b15ade286b9c4451ee675
iurii-chernigin <chernigin.y@gmail.com>
2023-03-19 18:12:36 +0300

    producer-base: implement readRideRecords method

--- src/main/java/kafka/producer/GreenRideProducer.java
+++ src/main/java/kafka/producer/GreenRideProducer.java
@@ -2,6 +2,7 @@ package kafka.producer;
 
 import schemaregistry.GreenRideRecord;
 
+import java.time.Instant;
 import java.util.Properties;
 import java.util.List;
 import java.util.Arrays;
@@ -23,14 +24,49 @@ public class GreenRideProducer {
         this.dataToProducePath = dataToProducePath;
     }
 
+    public void publishRidesToKafka(GreenRideRecord rideRecord) {
+        // Left field naming from source to be consistent,
+        // transformation will be at next steps
+        System.out.println(rideRecord.getDropoffLocationId());
+
+    }
+
+    /*
+     * Read csv with green taxi rides and publish them to Kafka topic
+     *
+     * Fields: VendorID, lpep_pickup_datetime, lpep_dropoff_datetime, PULocationID, DOLocationID
+     * Generated field: sended_to_kafka_ts
+     */
     public void readRideRecords() {
 
         var dataFileResource = this.getClass().getResource(dataToProducePath);
         try (CSVReader ridesCSVReader = new CSVReader(new FileReader(dataFileResource.getFile()))) {
-            List<String> csvHeaderArr = Arrays.asList(ridesCSVReader.readNext());
-            System.out.println(csvHeaderArr);
-            System.out.println("Position of DOLocationID is " + csvHeaderArr.indexOf("DOLocationID"));
-        } 
+
+
+            List<String> csvHeaderList = Arrays.asList(ridesCSVReader.readNext());
+            Integer pickupLocationIndex = csvHeaderList.indexOf("PULocationID");
+            Integer dropoffLocationIndex = csvHeaderList.indexOf("DOLocationID");
+            Integer vendorIDIndex = csvHeaderList.indexOf("VendorID");
+            Integer pickupDatetimeIndex = csvHeaderList.indexOf("lpep_pickup_datetime");
+            Integer dropoffDatetimeIndex = csvHeaderList.indexOf("lpep_dropoff_datetime");
+
+            ridesCSVReader
+                    .readAll()
+                    .stream()
+                    .map(
+                            row -> GreenRideRecord
+                                    .newBuilder()
+                                    .setVendorId(row[vendorIDIndex])
+                                    .setPickupLocationId(Integer.parseInt(row[pickupLocationIndex]))
+                                    .setDropoffLocationId(Integer.parseInt(row[dropoffLocationIndex]))
+                                    .setPickupDatetime(row[pickupDatetimeIndex])
+                                    .setDropoffDatetime(row[dropoffDatetimeIndex])
+                                    .setSendedToKafkaTs((int) Instant.now().getEpochSecond())
+                                    .build()
+                    )
+                    .forEach(row -> publishRidesToKafka(row));
+
+        }
         catch (CsvException csvException) {
             csvException.printStackTrace();
         } 
