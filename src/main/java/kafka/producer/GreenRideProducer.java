a7ff71c3f03b739e3082e91459bbdc5efba1d3a6
iurii-chernigin <chernigin.y@gmail.com>
2023-03-19 19:31:02 +0300

    producer-base: implement publishRidesToKafka method

--- src/main/java/kafka/producer/GreenRideProducer.java
+++ src/main/java/kafka/producer/GreenRideProducer.java
@@ -2,6 +2,9 @@ package kafka.producer;
 
 import schemaregistry.GreenRideRecord;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.time.Instant;
 import java.util.Properties;
 import java.util.List;
@@ -23,22 +26,34 @@ public class GreenRideProducer {
     
     private Properties kafkaConnectionProps;
     private String dataToProducePath;
+    private String greenRidesTopic;
 
-    public GreenRideProducer(String dataToProducePath) {
+    public GreenRideProducer(String dataToProducePath, String greenRidesTopic) {
         this.kafkaConnectionProps = KafkaConfig.getAvroProducerConfig();
         this.dataToProducePath = dataToProducePath;
+        this.greenRidesTopic = greenRidesTopic;
     }
 
     /**
      * Publish ride records to Kafka
      *
-     * @param rideRecord object that contains data about ride
-     */
-    public void publishRidesToKafka(GreenRideRecord rideRecord) {
-        // Left field naming from source to be consistent,
-        // transformation will be at next steps
-        System.out.println(rideRecord.getDropoffLocationId());
+     * @param rideRecord object contains data about one taxi ride (vendorID, pickupDatetime, etc)
+            */
+    public void publishRidesToKafka(KafkaProducer kafkaProducer, GreenRideRecord rideRecord) throws ExecutionException, InterruptedException {
 
+        var recordMeta = kafkaProducer.send(
+                new ProducerRecord<>(
+                        greenRidesTopic,
+                        String.valueOf(rideRecord.getPickupLocationId()),
+                        rideRecord
+                ),
+                (metadata, exception) -> {
+                    if (exception != null) {
+                        System.out.println(exception.getMessage());
+                    }
+                }
+        );
+        System.out.println(rideRecord.getPickupLocationId());
     }
 
     /**
@@ -47,46 +62,52 @@ public class GreenRideProducer {
      * Fields: VendorID, lpep_pickup_datetime, lpep_dropoff_datetime, PULocationID, DOLocationID
      * Generated field: sended_to_kafka_ts
      */
-    public void readRideRecords() {
+    public void readRideRecords() throws IOException, CsvException {
 
+        KafkaProducer<String, GreenRideRecord> kafkaProducer = new KafkaProducer<>(kafkaConnectionProps);
         var dataFileResource = this.getClass().getResource(dataToProducePath);
-        try (CSVReader ridesCSVReader = new CSVReader(new FileReader(dataFileResource.getFile()))) {
+        var ridesCSVReader = new CSVReader(new FileReader(dataFileResource.getFile()));
 
+        List<String> csvHeaderList = Arrays.asList(ridesCSVReader.readNext());
+        Integer pickupLocationIndex = csvHeaderList.indexOf("PULocationID");
+        Integer dropoffLocationIndex = csvHeaderList.indexOf("DOLocationID");
+        Integer vendorIDIndex = csvHeaderList.indexOf("VendorID");
+        Integer pickupDatetimeIndex = csvHeaderList.indexOf("lpep_pickup_datetime");
+        Integer dropoffDatetimeIndex = csvHeaderList.indexOf("lpep_dropoff_datetime");
 
-            List<String> csvHeaderList = Arrays.asList(ridesCSVReader.readNext());
-            Integer pickupLocationIndex = csvHeaderList.indexOf("PULocationID");
-            Integer dropoffLocationIndex = csvHeaderList.indexOf("DOLocationID");
-            Integer vendorIDIndex = csvHeaderList.indexOf("VendorID");
-            Integer pickupDatetimeIndex = csvHeaderList.indexOf("lpep_pickup_datetime");
-            Integer dropoffDatetimeIndex = csvHeaderList.indexOf("lpep_dropoff_datetime");
+        ridesCSVReader
+                .readAll()
+                .stream()
+                .map(
+                        row -> GreenRideRecord
+                                .newBuilder()
+                                .setVendorId(row[vendorIDIndex])
+                                .setPickupLocationId(Integer.parseInt(row[pickupLocationIndex]))
+                                .setDropoffLocationId(Integer.parseInt(row[dropoffLocationIndex]))
+                                .setPickupDatetime(row[pickupDatetimeIndex])
+                                .setDropoffDatetime(row[dropoffDatetimeIndex])
+                                .setSendedToKafkaTs((int) Instant.now().getEpochSecond())
+                                .build()
+                )
+                .forEach(row -> {
+                    try {
+                        publishRidesToKafka(kafkaProducer, row);
+                    } catch (ExecutionException e) {
+                        throw new RuntimeException(e);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
 
-            ridesCSVReader
-                    .readAll()
-                    .stream()
-                    .map(
-                            row -> GreenRideRecord
-                                    .newBuilder()
-                                    .setVendorId(row[vendorIDIndex])
-                                    .setPickupLocationId(Integer.parseInt(row[pickupLocationIndex]))
-                                    .setDropoffLocationId(Integer.parseInt(row[dropoffLocationIndex]))
-                                    .setPickupDatetime(row[pickupDatetimeIndex])
-                                    .setDropoffDatetime(row[dropoffDatetimeIndex])
-                                    .setSendedToKafkaTs((int) Instant.now().getEpochSecond())
-                                    .build()
-                    )
-                    .forEach(row -> publishRidesToKafka(row));
+        kafkaProducer.close();
 
-        }
-        catch (CsvException csvException) {
-            csvException.printStackTrace();
-        } 
-        catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
     }
 
-    public static void main(String[] args) {
-        var greenRidesProducer = new GreenRideProducer("/sample_green_tripdata_2019-01.csv");
+    public static void main(String[] args) throws IOException, CsvException {
+        var greenRidesProducer = new GreenRideProducer(
+                "/sample_green_tripdata_2019-01.csv",
+                "rides_green"
+        );
         greenRidesProducer.readRideRecords();
     }
 
