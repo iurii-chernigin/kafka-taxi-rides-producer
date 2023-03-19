4b57ec1c6fe13fffd15bbb6f1272d6b0bf1068ea
iurii-chernigin <chernigin.y@gmail.com>
2023-03-19 20:02:15 +0300

    base: refresh gitignore

--- src/main/java/kafka/producer/KafkaSecrets.java
+++ src/main/java/kafka/producer/KafkaSecrets.java
@@ -2,9 +2,9 @@ package kafka.producer;
 
 public class KafkaSecrets {
 
-    public static final String KAFKA_CLUSTER_KEY = "";
-    public static final String KAFKA_CLUSTER_SECRET = "";
+    public static final String KAFKA_CLUSTER_KEY = "MWZJN5JIG36NXMA5";
+    public static final String KAFKA_CLUSTER_SECRET = "svni6ehe8DaUDvPwEGkYiL64JjNeKO8FlbBAkVO0Vctynt6RhUmclmNZ5Y0VdCHv";
 
-    public static final String SCHEMA_REGISTRY_KEY = "";
-    public static final String SCHEMA_REGISTRY_SECRET = "";
+    public static final String SCHEMA_REGISTRY_KEY = "UZP2AIYZDRTCXERO";
+    public static final String SCHEMA_REGISTRY_SECRET = "DTR6h1UTjkBUhrdBj6Hwhkac08h9T+0nxHf71+cg8be+KBnYp4ZnqzD3FRgJNAAs";
 }
